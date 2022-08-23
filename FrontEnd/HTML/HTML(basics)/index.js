e0adeb49cea4a1fbb477a7e6d7e676e0d82a023d
ayansh1729 <cs21b2012@iiitdm.ac.in>
2022-08-23 23:55:46 +0530

    first commit(introduced Beef basic)

---
+++ FrontEnd/HTML/HTML(basics)/index.js
@@ -0,0 +1,7 @@
+console.log(
+  "The tab is now hooked to this file...if we make changes in this file...we can pretty much conrol everything"
+);
+document.getElementById("hook").innerHTML =
+  "The tab is now hooked to this file...if we make changes in this file...we can pretty much conrol everything";
+//After being hooked just change the index.js file and display whatever you want at your page
+alert("You're hacked");
